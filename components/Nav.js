35c9426b779f2f02cc2724348bb77b671798aa17
mohamedeissa448 <mohamedeissa448@gmail.com>
2022-09-23 14:59:41 +0200

    disapperar protected urls from navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,32 @@
-import Link from 'next/link'
-import navStyles from '../styles/Nav.module.css'
+import Link from 'next/link';
+import navStyles from '../styles/Nav.module.css';
+import isUserLoggedIn from './helpers/isUserLogged';
+import React, { useEffect, useState  } from 'react';
 
 const Nav = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    setIsLoggedIn(isUserLoggedIn());
+}, [])
+
   return (
     <nav className={navStyles.nav}>
       <ul>
       <li>
           <Link href='/'>Log In</Link>
         </li>
-        <li>
-          <Link href='/users'>Users</Link>
-        </li>
-        <li>
-          <Link href='/articles'>My Articles</Link>
-        </li>
+        {isLoggedIn ? <>
+            <li>
+              <Link href='/users'>Users</Link>
+            </li>
+            <li>
+              <Link href='/articles'>My Articles</Link>
+            </li>
+            </>:
+        ''
+        }
+        
       </ul>
     </nav>
   )
